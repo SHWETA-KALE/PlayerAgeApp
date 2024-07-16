c9c8ff606b9999d17c3c7f1ecb955e7fcac99403
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-16 12:29:22 +0530

    PlayerApp

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using PlayerApp.models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[]
+            {
+                new Player(101, "A", 29),
+                new Player(101, "B", 20),
+                new Player(101, "C", 30),
+                new Player(101, "D", 34),
+                new Player(210, "E")
+            };
+
+             Player eldest = Player.WhoIsElder(players);
+             Console.WriteLine(eldest);
+
+
+        }
+    }
+}
---
+++ models/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerApp.models;
+using System.Xml.Linq;
+using System.Runtime.CompilerServices;
+
+namespace PlayerApp.models
+{
+    internal class Player
+    {
+
+
+        private const int DEFAULT_AGE = 18;
+
+        public int Id { get;  }
+        public string Name { get ; } 
+        
+        public int Age { get; } 
+
+//constructor
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Age = DEFAULT_AGE;
+        }
+        public Player(int id, string name, int age): this(id, name)
+        {
+            Age = age;
+        }
+
+        public static Player WhoIsElder(Player[]players)
+        {
+            Player elderPlayerAge = null;
+            int elder = DEFAULT_AGE;
+            foreach (Player player in players) {
+
+                if (player.Age> elder)
+                {
+                    elder = player.Age;
+                    elderPlayerAge =player;
+                }
+            }
+            return elderPlayerAge;
+
+
+    }
+        public override string ToString()
+        {
+            return $"Eldest Id: {Id}\n" +
+                $"Eldest Player Name: {Name}\n" +
+                $"Age: {Age}" ;
+        }
+
+
+
+    }
+}
